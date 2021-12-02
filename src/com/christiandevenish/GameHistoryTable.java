789c85ac3e2d4113c14e71876bad15fb491e62d3
Christian Devenish <devenishchristian@gmail.com>
2021-12-01 22:45:06 -0600

    Basic GUI complete

---
+++ src/com/christiandevenish/GameHistoryTable.java
@@ -0,0 +1,156 @@
+package com.christiandevenish;
+
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameHistoryTable extends JScrollPane {
+
+    private final DataModel dataModel;
+    private final JTable table;
+    private final JScrollPane scrollPane;
+
+    public GameHistoryTable(int width, int height) {
+        dataModel = new DataModel();
+        table = new JTable(dataModel);
+        table.setRowHeight(15);
+        scrollPane = new JScrollPane(table);
+        scrollPane.setColumnHeaderView(table.getTableHeader());
+        scrollPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setPreferredSize(new Dimension(width, height));
+    }
+
+    /*
+    public void redo(Board board, MoveLog moveLog) {
+        int currentRow = 0;
+        this.dataModel.clear();
+        for (Move move : moveLog.getMoves()) {
+            String moveText = move.toString();
+            if (move.getMovedPiece().getPieceColor() == PlayerColor.WHITE) {
+                this.dataModel.setValueAt(moveText, currentRow, 0);
+            } else if (move.getMovedPiece().getPieceColor() == PlayerColor.BLACK) {
+                this.dataModel.setValueAt(moveText, currentRow, 0);
+                currentRow++;
+            }
+        }
+
+        if (moveLog.getMoves().size() > 0) {
+            Move lastMove = moveLog.getMoves().get(moveLog.getMoves().size() - 1);
+            String moveText = lastMove.toString();
+
+            if (lastMove.getMovedPiece().getPieceColor() == PlayerColor.WHITE) {
+                this.dataModel.setValueAt(moveText + calculateCheckAndCheckMateHash(), currentRow - 1, 1);
+            }
+        }
+
+        JScrollBar vertical = scrollPane.getVerticalScrollBar();
+        vertical.setValue(vertical.getMaximum());
+    }
+     */
+
+    /*
+    private String calculateCheckAndCheckMateHash(Game game) {
+        if (game.currentPlayer().isInCheckmate()) {
+            return "#";
+        } else if (game.currentPlayer().isInCheck()) {
+            return "+";
+        }
+        return "";
+    }
+    */
+
+    public JScrollPane getScrollPane() {
+        return this.scrollPane;
+    }
+
+    private class DataModel extends DefaultTableModel {
+        private final List<Row> values;
+        private final String[] NAMES = {"White","Black"};
+
+        protected DataModel() {
+            this.values = new ArrayList<>();
+        }
+
+        public void clear() {
+            this.values.clear();
+            setRowCount(0);
+        }
+
+        @Override
+        public int getRowCount() {
+            if (this.values == null) {
+                return 0;
+            }
+            return this.values.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return NAMES.length;
+        }
+
+        @Override
+        public Object getValueAt(int row, int column) {
+            Row currentRow = this.values.get(row);
+            if (column == 0) {
+                return currentRow.getWhiteMove();
+            } else if (column == 1) {
+                return currentRow.getBlackMove();
+            }
+            return null;
+        }
+
+        @Override
+        public void setValueAt(Object aValue, int row, int column) {
+            Row currentRow;
+            if (this.values.size() <= row) {
+                currentRow = new Row();
+                this.values.add(currentRow);
+            } else {
+                currentRow = this.values.get(row);
+            }
+            if (column == 0) {
+                currentRow.setWhiteMove((String) aValue);
+            } else if (column == 1) {
+                currentRow.setBlackMove((String) aValue);
+                fireTableCellUpdated(row, column);
+            }
+        }
+
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            return Move.class;
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            return NAMES[column];
+        }
+    }
+
+    private class Row {
+        private String whiteMove;
+        private String blackMove;
+
+        public String getWhiteMove() {
+            return whiteMove;
+        }
+
+        public void setWhiteMove(String whiteMove) {
+            this.whiteMove = whiteMove;
+        }
+
+        public String getBlackMove() {
+            return blackMove;
+        }
+
+        public void setBlackMove(String blackMove) {
+            this.blackMove = blackMove;
+        }
+    }
+
+}
--- src/com/christiandevenish/Window.java
+++ src/com/christiandevenish/Window.java
@@ -1,44 +1,41 @@
 package com.christiandevenish;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Window extends JFrame {
-
-    private final Board board;
 
     private final JPanel mainPanel;
     private final BoardCanvas boardCanvas;
 
     public Window(Board board) {
         super("Chess");
-        this.mainPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        this.mainPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
         this.mainPanel.setPreferredSize(new Dimension(900, 600));
         this.setContentPane(mainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setResizable(false);
         this.setLocationRelativeTo(null);
-        this.setVisible(true);
+        try {
+            this.setIconImage(ImageIO.read(new File("res/window_icon/pizza.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         this.boardCanvas = new BoardCanvas(board);
         this.boardCanvas.setPreferredSize(new Dimension(600, 600));
         this.mainPanel.add(boardCanvas);
 
-        // ADD COMPONENT FOR MOVE LIST WTF??? SOMETIMES WORKS OTHER TIMES NO??
-        Canvas canvas = new Canvas() {
-            @Override
-            public void paint(Graphics g) {
-                g.setColor(Color.BLACK);
-                g.fillRect(0, 0, getWidth(), getHeight());
-                System.out.println(getWidth() + ", " + getHeight());
-                System.out.println("here");
-            }
-        };
-        canvas.setPreferredSize(new Dimension(300, 600));
-        this.mainPanel.add(canvas);
+        // ADD COMPONENT FOR SIDE MENU (WORKING!!!)
+        GameHistoryTable gameHistoryTable = new GameHistoryTable(300, 600);
+        this.mainPanel.add(gameHistoryTable.getScrollPane());
 
-        this.board = board;
+        this.setVisible(true);
+
     }
 }
